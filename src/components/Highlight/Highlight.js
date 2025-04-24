d34cee5a89fc124a2cef322a27a9fa3777bd0b3c
andreavenue <dev.marto87@gmail.com>
2025-04-24 14:06:52 -0300

    create DishCard component

--- src/components/Highlight/Highlight.js
+++ src/components/Highlight/Highlight.js
@@ -1,5 +1,10 @@
 import "./Highlight.css";
 import Title from "../Title/Title";
+import DishCard from "../DishCard/DishCard";
+import GreekSalad from "../../assets/images/greek-salad.png";
+import Bruschetta from "../../assets/images/bruschetta.png";
+import LemonDessert from "../../assets/images/lemon-dessert.png";
+
 function Highlight(){
   return(
     <section className="container">
@@ -17,6 +22,26 @@ function Highlight(){
               <path d="M18.6666 9.3335L25.3333 16.0002" stroke="#F4CE14" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
             </svg>
           </a>
+        </div>
+        <div className="highlight-dishes">
+          <DishCard
+            title="Greek Salad"
+            description="A bright and flavorful Greek Salad, bursting with fresh vegetables, salty feta, and a drizzle of olive oil."
+            price="$ 9.99"
+            image={GreekSalad}
+          />
+          <DishCard
+            title="Bruchetta"
+            description="A delightful harmony of toasted bread, ripe tomatoes, fragrant basil, and a drizzle of balsamic glaze."
+            price="$ 9.99"
+            image={Bruschetta}
+          />
+          <DishCard
+            title="Lemon Dessert"
+            description="A wonderfully refreshing Lemon Dessert, the perfect ending to any meal with its light and lively citrus notes that cleanse the palate."
+            price="$ 9.99"
+            image={LemonDessert}
+          />
         </div>
       </div>
     </section>
