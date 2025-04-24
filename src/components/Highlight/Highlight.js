ac89eefd1eed132c54f109cdc215d3277f2ee583
andreavenue <dev.marto87@gmail.com>
2025-04-24 17:47:48 -0300

    install swiper

--- src/components/Highlight/Highlight.js
+++ src/components/Highlight/Highlight.js
@@ -15,7 +15,7 @@ function Highlight(){
             description="Discover a perfect blend of local tradition and modern culinary art, made with the freshest ingredients."
           />
           <a href="#" className="highlight-button">
-            Explore our full menu menu 
+            Explore our full menu 
             <svg width="32" height="29" viewBox="0 0 32 29" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M25.3333 16.0002H6.66663" stroke="#F4CE14" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
               <path d="M18.6666 22.6667L25.3333 16" stroke="#F4CE14" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
--- src/components/Testimonial/Testimonial.js
+++ src/components/Testimonial/Testimonial.js
@@ -1,16 +1,39 @@
+import "./Testimonial.css";
+import Title from "../Title/Title";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+
 function Testimonial(){
   return (
-    <section>
-      <p>Testimonial</p>
-      <article>
-        Testimonial 1
-      </article>
-      <article>
-        Testimonial 2
-      </article>
-      <article>
-        Testimonial 3
-      </article>
+    <section className="testimonial-container">
+      <div className="container">
+        <div className="testimonial-header">
+          <Title 
+            title="Our customer say" 
+            description="Illuminate your path to success with insights from our customer spotlights." 
+          />
+        </div>
+
+
+
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={3}
+          onSlideChange={() => console.log('slide change')}
+          onSwiper={(swiper) => console.log(swiper)}
+        >
+          <SwiperSlide>Slide 1</SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+        </Swiper>
+
+
+      </div>
+      
     </section>
   )
 }
